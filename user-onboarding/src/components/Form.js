bc4202f0454de1febedf99e72ca9afceb5a5a97a
unknown <ryancare@protonmail.com>
2022-03-30 18:17:51 -0500

    Added form.js, imported needful things, created form styled component,added form state and submit button with disabled option

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,23 @@
+import {useState,useEffect} from 'react';
+import {styled} from 'styled-components';
+import * as Yup from 'yup';
+import formSchema from './metaland/formSchema.js';
+
+const Former = styled.form`
+`
+
+const Form = (props) => {
+  const [formState,setFormState] = useState({
+      name:"",
+      email:"",
+      password:"",
+      terms:"",
+      submitDisabled:true
+  })
+  return (
+    <Former>
+        <button disabled={formState.submitDisabled}>Submit!</button>
+    </Former>
+  )
+}
+export default Form;
